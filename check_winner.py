62a90a1e5df0c0d04d8083e91163a2582f3a1b5c
Conner4348 <92555576+Conner4348@users.noreply.github.com>
2023-09-27 15:46:47 -0400

    Created check_winner.py

---
+++ check_winner.py
@@ -0,0 +1,13 @@
+# ==================================
+# FILE FOR FUNCTION THAT CHECKS
+# TO SEE IF THERE IS A WINNER, BASED
+# ON THE CURRENT GAME STATE.
+# ==================================
+
+# Create a function that will check if there is a winner based
+# on some test inputs.
+# Example:
+# input: [[X, X, X], [3, O, O], [6, X, O]]
+# The function should look at this input and return if one of the sublists
+# has all of the same value.
+# Should return True, because the first sublist's values are all equal.
