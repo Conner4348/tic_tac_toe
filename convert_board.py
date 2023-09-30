e0bdd4f6ca345e9fbfcfa4179b0345425ddeb820
Conner4348 <92555576+Conner4348@users.noreply.github.com>
2023-09-30 11:46:48 -0400

    Created convert_board function

--- convert_board.py
+++ convert_board.py
@@ -10,4 +10,35 @@
 
 def convert_board(game_state, config):
 
-  pass
+  board_config = []
+
+  if config == 0:
+    current_sub = []
+    for val in game_state.values():
+      current_sub.append(val)
+      if len(current_sub) == 3:
+        board_config.append(current_sub)
+        current_sub = []
+    print(board_config)
+    return board_config
+
+  if config == 1:
+    current_sub = []
+    current_index = 0
+    for key, val in game_state.items():
+      pass
+      # WRITE PSEUDOCODE FOR THIS PART
+      # BEFORE TRYING TO TACKLE REST.
+
+  if config == 2:
+    pass
+
+
+
+# =======
+# TESTING
+# =======
+
+test_one = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8}
+
+fn_test_one = convert_board(test_one, 0)
