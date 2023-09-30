148c5c496f32f5b937aa02ecc65f25d1e15ebfd8
Conner4348 <92555576+Conner4348@users.noreply.github.com>
2023-09-30 11:29:12 -0400

    Create convert_board.py

---
+++ convert_board.py
@@ -0,0 +1,13 @@
+# ===================================================
+# THIS FILE WILL HOLD A FUNCTION THAT
+# TAKES A DICTIONARY SHOWING THE CURRENT GAME STATE
+# AND WILL CONVERT IT INTO A LIST CONTAINING SUBLISTS
+# FOR EITHER EACH ROW, COLUMN OR DIAGONAL.
+# ===================================================
+
+# Create a function that takes the game state and
+# how you want the board to be configured (0=across, 1=vertical, 2=diagonal).
+
+def convert_board(game_state, config):
+
+  pass
