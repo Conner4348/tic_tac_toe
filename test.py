d01d2fe0ad5e7a185541e0e14c01196e2ccca3e0
Conner4348 <92555576+Conner4348@users.noreply.github.com>
2023-10-03 09:56:15 -0400

    Created part of function for diagonals and test.py

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 # ==================================================
 # THIS FILE WILL TEST THE FILES ALTOGETHER THUS FAR.
 # ==================================================
+
+from check_winner import check_winner as cw
+from convert_board import convert_board as convert
+from get_input import get_input as get
+from get_input import bot_input as bot
+
+game_state = {0: None, 1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None}
+
+get(game_state)
+cw(convert(game_state, 0), 'X')
+cw(convert(game_state, 1), 'X')
+cw(convert(game_state, 2), 'X')
+bot(game_state)
+cw(convert(game_state, 0), 'O')
+cw(convert(game_state, 1), 'O')
+cw(convert(game_state, 2), 'O')
